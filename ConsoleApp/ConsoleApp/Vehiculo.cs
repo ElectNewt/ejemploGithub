22c3dce2ac3d15ba358cc4d8007d04c359c71b33
ivan abad <ivan.abad92@gmail.com>
2019-05-15 15:47:23 +0100

    tarea 1

---
+++ ConsoleApp/ConsoleApp/Vehiculo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    class Vehiculo
+    {
+        public decimal VelocidadMaxima { get; set; }
+
+        public Vehiculo(decimal velocidadMaxima)
+        {
+            VelocidadMaxima = velocidadMaxima;
+        }
+
+    }
+}
